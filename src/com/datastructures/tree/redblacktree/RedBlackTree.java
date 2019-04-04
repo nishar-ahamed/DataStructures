84e8896118988a4a332d654e3fd957d6531dee98
nishar-ahamed <nishar.kwd@gmail.com>
2019-04-04 23:31:10 +0530

    Adding balancing code

--- src/com/datastructures/tree/redblacktree/RedBlackTree.java
+++ src/com/datastructures/tree/redblacktree/RedBlackTree.java
@@ -19,13 +19,64 @@ public class RedBlackTree {
             root.setLeftNode(insert(root.getLeftNode(), node));
             root.getLeftNode().setParent(root);
         } else if (node.getData() > root.getData()) {
-            root.setRightNode(insert(root.getLeftNode(), node));
+            root.setRightNode(insert(root.getRightNode(), node));
             root.getRightNode().setParent(root);
         }
         return root;
     }
 
     private void balanceAfterInsertion(Node node) {
+        Node parentNode = null;
+        Node grandParentNode = null;
+
+        while (node != root && node.getColor() != NodeColor.BLACK && node.getParent().getColor() == NodeColor.RED) {
+            parentNode = node.getParent();
+            grandParentNode = node.getParent().getParent();
+            if (parentNode == grandParentNode.getLeftNode()) {
+                Node uncle = grandParentNode.getRightNode();
+                if (uncle != null && uncle.getColor() == NodeColor.RED) {
+                    grandParentNode.setColor(NodeColor.RED);
+                    parentNode.setColor(NodeColor.BLACK);
+                    uncle.setColor(NodeColor.BLACK);
+                    node = grandParentNode;
+                } else {
+
+                    if (node == parentNode.getRightNode()) {
+                        leftRotate(parentNode);
+                        node = parentNode;
+                        parentNode = node.getParent();
+                    }
+                    rightRotate(grandParentNode);
+                    NodeColor temColor = parentNode.getColor();
+                    parentNode.setColor(grandParentNode.getColor());
+                    grandParentNode.setColor(temColor);
+                    node = parentNode;
+                }
+
+            } else {
+                Node uncle = grandParentNode.getLeftNode();
+                if (uncle != null && uncle.getColor() == NodeColor.RED) {
+                    grandParentNode.setColor(NodeColor.RED);
+                    parentNode.setColor(NodeColor.BLACK);
+                    uncle.setColor(NodeColor.BLACK);
+                    node = grandParentNode;
+                } else {
+                    if (node == parentNode.getLeftNode()) {
+                        rightRotate(parentNode);
+                        node = parentNode;
+                        parentNode = node.getParent();
+                    }
+                    leftRotate(grandParentNode);
+                    NodeColor temColor = parentNode.getColor();
+                    parentNode.setColor(grandParentNode.getColor());
+                    grandParentNode.setColor(temColor);
+                    node = parentNode;
+                }
+            }
+        }
+        if (root.getColor() == NodeColor.RED) {
+            root.setColor(NodeColor.BLACK);
+        }
     }
 
     private void rightRotate(Node node) {
@@ -92,6 +143,9 @@ public class RedBlackTree {
     }
 
     public static void main(String[] args) {
-
+        RedBlackTree rbt = new RedBlackTree();
+        rbt.insert(10);
+        rbt.insert(15);
+        rbt.insert(13);
     }
 }
